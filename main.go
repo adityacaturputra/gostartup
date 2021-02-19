9a965351c431286a985a4a64b4718263e08d3731
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-19 16:23:06 +0700

    0204 Gorm get table to json

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"gostartup/user"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,6 +13,32 @@ import (
 func main() {
 
 	// didapatkan dari:   https://gorm.io/docs/connecting_to_the_database
+	// dsn := "root:@tcp(127.0.0.1:3306)/gostartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection to database is good")
+
+	// var users []user.User
+
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println("Id:", user.ID)
+	// 	fmt.Println("Name:", user.Name)
+	// 	fmt.Println("Email:", user.Email)
+	// 	fmt.Println("================")
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/gostartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -19,27 +46,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is good")
-
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
-
 	db.Find(&users)
-	length = len(users)
-
-	fmt.Println(length)
-	for _, user := range users {
-		fmt.Println("Id:", user.ID)
-		fmt.Println("Name:", user.Name)
-		fmt.Println("Email:", user.Email)
-		fmt.Println(user.Occupation)
-		fmt.Println(user.PasswordHash)
-		fmt.Println(user.Role)
-		fmt.Println(user.AvatarFileName)
-		fmt.Println(user.CreatedAt)
-		fmt.Println(user.UpdatedAt)
-		fmt.Println("================")
-	}
+	c.JSON(http.StatusOK, users)
 }
