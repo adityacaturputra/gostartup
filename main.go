4fbef36ce89a792915b48fb9fad3d0a33264523e
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-19 15:40:17 +0700

    0202 Gorm connection to db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gostartup/user"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -12,11 +13,33 @@ func main() {
 
 	// didapatkan dari:   https://gorm.io/docs/connecting_to_the_database
 	dsn := "root:@tcp(127.0.0.1:3306)/gostartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database is good")
+
+	var users []user.User
+	length := len(users)
+
+	fmt.Println(length)
+
+	db.Find(&users)
+	length = len(users)
+
+	fmt.Println(length)
+	for _, user := range users {
+		fmt.Println("Id:", user.ID)
+		fmt.Println("Name:", user.Name)
+		fmt.Println("Email:", user.Email)
+		fmt.Println(user.Occupation)
+		fmt.Println(user.PasswordHash)
+		fmt.Println(user.Role)
+		fmt.Println(user.AvatarFileName)
+		fmt.Println(user.CreatedAt)
+		fmt.Println(user.UpdatedAt)
+		fmt.Println("================")
+	}
 }
