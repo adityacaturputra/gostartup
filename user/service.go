3d79b7b6406efed640e9ee1a099c8c5633b75064
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-22 15:20:24 +0700

    0502 Email check - service

--- user/service.go
+++ user/service.go
@@ -9,6 +9,7 @@ import (
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
+	IsEmailAvailable(input CheckEmailInput) (bool, error)
 }
 
 type service struct {
@@ -47,7 +48,7 @@ func (s *service) Login(input LoginInput) (User, error) {
 	user, err := s.repository.FindByEmail(email)
 
 	if err != nil {
-		return user, nil
+		return user, err
 	}
 
 	if user.ID == 0 {
@@ -61,4 +62,19 @@ func (s *service) Login(input LoginInput) (User, error) {
 
 	return user, nil
 
+}
+
+func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error) {
+	email := input.Email
+
+	user, err := s.repository.FindByEmail(email)
+	if err != nil {
+		return false, err
+	}
+
+	if user.ID == 0 {
+		return true, nil
+	}
+
+	return false, nil
 }
