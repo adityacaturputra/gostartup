6373123a5c9d151e28126f9750e553ed26a8e000
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-03-05 10:21:39 +0700

    1203. Create Campaign -  Service CreateCampaign method and CreateCampaignInput struct

--- campaign/service.go
+++ campaign/service.go
@@ -3,6 +3,7 @@ package campaign
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -36,4 +37,23 @@ func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error
 		return campaign, err
 	}
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.GoalAmount = input.GoalAmount
+	campaign.Perks = input.Perks
+	campaign.UserID = input.User.ID
+
+	// pembuatan slug (skip dulu)
+
+	savedCampaign, err := s.repository.Save(campaign)
+	if err != nil {
+		return savedCampaign, err
+	}
+	return savedCampaign, nil
+
 }
